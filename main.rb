564bc1b7bb8b47c5511fff9d9209d176d2fd55ec
tania63 <tindjova@abv.bg>
2014-06-13 22:01:35 +0300

    Start numerology

---
+++ main.rb
@@ -0,0 +1,21 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+puts "Дата на раждане"
+date = gets.chomp
+puts "Месец на раждане"
+month = gets.chomp
+puts "Година на раждане"
+year = gets.chomp
+
+sum = date.to_i + month.to_i + year.to_i
+
+while sum > 9 do 
+	numbers_string = sum.to_s.split("")
+	sum = 0
+	numbers = numbers_string.map{ |number| number.to_i }
+	numbers.each{ |number| sum += number }
+end
+
+puts "Пътят на живота е #{sum}"
+
