ad4ccad1cfa29b5882fd894bf2ed6cf50b6680a9
tania63 <tindjova@abv.bg>
2014-07-02 23:02:23 +0300

    introduction of Ruby code in html

--- numerologia.rb
+++ numerologia.rb
@@ -2,15 +2,31 @@
 # encoding: utf-8
 
 require 'sinatra'
+result = { 
+		1 => '1',
+		2 => '2',
+		3 => '3',
+		4 => '4',
+		5 => '5',
+		6 => '6',
+		7 => '7',
+		8 => '8',
+		9 => '9',
+	}
+	
+get '/' do
+	erb :numerology_template_new
+end
+
 
 post '/' do
 	p request.body
 	puts "Дата на раждане"
-	date = gets.chomp
+	date = params[birth_date]
 	puts "Месец на раждане"
-	month = gets.chomp
+	month = params[birth_month]
 	puts "Година на раждане"
-	year = gets.chomp
+	year = params[birth_year]
 
 	sum = date.to_i + month.to_i + year.to_i
 
@@ -25,6 +41,7 @@ post '/' do
 
 
 end
+
 
 
 
